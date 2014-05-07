d146afe918b867ecb8a606c18157ef71bdced1d9
fschmid <fs@studer-raimann.ch>
2014-05-07 07:12:55 +0000

    git-svn-id: https://svn.ilias.de/svn/ilias/branches/sr/Plugins/Hub_trunk@49888 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/Configuration/class.hubConfig.php
+++ classes/Configuration/class.hubConfig.php
@@ -6,6 +6,7 @@ require_once('./Customizing/global/plugins/Libraries/ActiveRecord/class.ActiveRe
  *
  * @author  Fabian Schmid <fs@studer-raimann.ch>
  * @version 1.1.02
+ * 
  */
 class hubConfig extends ActiveRecord {
 
