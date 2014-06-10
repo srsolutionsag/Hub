004ff62e8a186eb98d1401483a9cc9d34b94fb97
fschmid <fs@studer-raimann.ch>
2014-06-10 13:22:03 +0000

    git-svn-id: https://svn.ilias.de/svn/ilias/branches/sr/Plugins/Hub_trunk@50631 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/Connector/class.hubConnector.php
+++ classes/Connector/class.hubConnector.php
@@ -21,23 +21,29 @@ class hubConnector extends arConnectorDB {
 	 * @return ilDB
 	 */
 	public function returnDB() {
-		if (hubConfig::get(hubConfig::F_DB)) {
-			if (! isset(self::$db_cache)) {
-				$database = ilDBWrapperFactory::getWrapper('mysql');
-				$database->setDBHost(hubConfig::get(hubConfig::F_DB_HOST));
-				$database->setDBName(hubConfig::get(hubConfig::F_DB_NAME));
-				$database->setDBUser(hubConfig::get(hubConfig::F_DB_USER));
-				$database->setDBPassword(hubConfig::get(hubConfig::F_DB_PASSWORD));
-				$a_port = hubConfig::get(hubConfig::F_DB_PORT) ? hubConfig::get(hubConfig::F_DB_PORT) : 3306;
-				$database->setDBPort($a_port);
-				$database->connect();
-				self::$db_cache = $database;
+		global $ilDB;
+		/**
+		 * @var $ilDB ilDB
+		 */
+		if ($ilDB->tableExists(hubConfig::returnDbTableName())) {
+			if (hubConfig::get(hubConfig::F_DB)) {
+				if (! isset(self::$db_cache)) {
+					$database = ilDBWrapperFactory::getWrapper('mysql');
+					$database->setDBHost(hubConfig::get(hubConfig::F_DB_HOST));
+					$database->setDBName(hubConfig::get(hubConfig::F_DB_NAME));
+					$database->setDBUser(hubConfig::get(hubConfig::F_DB_USER));
+					$database->setDBPassword(hubConfig::get(hubConfig::F_DB_PASSWORD));
+					$a_port = hubConfig::get(hubConfig::F_DB_PORT) ? hubConfig::get(hubConfig::F_DB_PORT) : 3306;
+					$database->setDBPort($a_port);
+					$database->connect();
+					self::$db_cache = $database;
+				}
+
+				return self::$db_cache;
+			} else {
+
+				return parent::returnDB();
 			}
-
-			return self::$db_cache;
-		} else {
-
-			return parent::returnDB();
 		}
 	}
 }
