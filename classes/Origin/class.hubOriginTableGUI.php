4498d83d5e32d9918afda72e488d57e6e536f61c
fschmid <fs@studer-raimann.ch>
2014-06-18 09:11:13 +0000

    [Hub] Bugfixes
    git-svn-id: https://svn.ilias.de/svn/ilias/branches/sr/Plugins/Hub_trunk@50813 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/Origin/class.hubOriginTableGUI.php
+++ classes/Origin/class.hubOriginTableGUI.php
@@ -73,9 +73,9 @@ class hubOriginTableGUI extends srModelObjectTableGUI {
 		$this->setFormAction($this->ctrl->getFormAction($this->parent_obj));
 		if (hubConfig::get(hubConfig::F_USE_ASYNC)) {
 			$this->addCommandButton('runAsync', $this->pl->txt('origin_table_button_run') . ' (Async)');
-			$this->addCommandButton('run', $this->pl->txt('origin_table_button_run'));
-			$this->addCommandButton('dryRun', $this->pl->txt('origin_table_button_dryrun'));
 		}
+		$this->addCommandButton('run', $this->pl->txt('origin_table_button_run'));
+		$this->addCommandButton('dryRun', $this->pl->txt('origin_table_button_dryrun'));
 		$this->addCommandButton('deactivateAll', $this->pl->txt('origin_table_button_deactivate_all'));
 		$this->addCommandButton('activateAll', $this->pl->txt('origin_table_button_activate_all'));
 	}
