3cdcb23b0a172d91a7d40e1b8d78d7ef82dedc26
Timon Amstutz <timon.amstutz@bluewin.ch>
2014-09-11 12:33:23 +0200

    Performance Improvement 2

--- classes/Course/class.hubCourseIndexTableGUI.php
+++ classes/Course/class.hubCourseIndexTableGUI.php
@@ -3,7 +3,6 @@ require_once('./Customizing/global/plugins/Libraries/ActiveRecord/Views/Index/cl
 require_once('./Customizing/global/plugins/Libraries/ActiveRecord/Views/Index/class.arIndexTableGUI.php');
 require_once('./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/Hub/classes/Sync/class.hubSyncHistory.php');
 require_once('./Services/Link/classes/class.ilLink.php');
-
 /**
  * TableGUI hubCourseIndexTableGUI
  *
@@ -11,8 +10,7 @@ require_once('./Services/Link/classes/class.ilLink.php');
  * @version 1.1.04
  *
  */
-class hubCourseIndexTableGUI extends arIndexTableGUI
-{
+class hubCourseIndexTableGUI extends arIndexTableGUI {
 
     protected function initToolbar()
     {
@@ -41,7 +39,7 @@ class hubCourseIndexTableGUI extends arIndexTableGUI
         $field->setSortable(true);
         $field->setPosition(30);
 
-        $field = new arIndexTableField("status", "status", "text", 40, true, false, false);
+        $field= new arIndexTableField("status","status","text", 40,true,false,false);
         $this->addField($field);
     }
 
@@ -50,10 +48,13 @@ class hubCourseIndexTableGUI extends arIndexTableGUI
         switch ($field->getName())
         {
             case 'title':
-                return '<a target=\'_blank\' href=\'' . ilLink::_getLink($item['ilias_id']) . '\'>' . $item['title_prefix'] . $value . '</a>';
+                $hubCourse      = hubCourse::find($item['ext_id']);
+                $hubSyncHistory = hubSyncHistory::find($item['ext_id']);
+                return '<a target=\'_blank\' href=\'' . ilLink::_getLink($hubSyncHistory->getIliasId()) . '\'>' . $hubCourse->getTitlePrefix() . $value . '</a>';
                 break;
             case 'parent_id':
-                return '<a target=\'_blank\' href=\'' . ilLink::_getLink($item['parent_id']) . '\'>' . $item['obj_title'] . '</a>';
+                $hubCourse = hubCourse::find($item['ext_id']);
+                return '<a target=\'_blank\' href=\'' . ilLink::_getLink($hubCourse->getParentId()) . '\'>' . ilObject2::_lookupTitle(ilObject2::_lookupObjId($hubCourse->getParentId())) . '</a>';
                 break;
             default:
                 return parent::setArFieldData($field, $item, $value);
@@ -61,28 +62,21 @@ class hubCourseIndexTableGUI extends arIndexTableGUI
         }
     }
 
-    protected function setCustomFieldData(arIndexTableField $field, $item)
-    {
+    protected function setCustomFieldData(arIndexTableField $field, $item){
         $hubSyncHistory = hubSyncHistory::find($item['ext_id']);
         return $this->txt('common_status_' . $hubSyncHistory->getTemporaryStatus());
     }
 
-    protected function beforeGetData()
-    {
-        $this->active_record_list->innerjoin("sr_hub_sync_history", "sr_hub_course.ext_id", "ext_id", array("ilias_id"));
-        $this->active_record_list->innerjoin("object_reference", "parent_id", "ref_id", array("obj_id"));
-        $this->active_record_list->innerjoin("object_data", "object_reference.obj_id", "obj_id", array("title AS obj_title"));
-    }
-
     protected function addCustomFilterWhere($type, $name, $value)
     {
-        if ($name == "parent_id")
+        if($name == "parent_id")
         {
+            $this->active_record_list->innerjoin("object_reference","parent_id","ref_id",array("obj_id"));
+            $this->active_record_list->innerjoin("object_data","object_reference.obj_id","obj_id", array("title AS obj_title"));
             $this->active_record_list->where("object_data.title like '%" . $value . "%'");
             return true;
         }
         return false;
     }
 }
-
 ?>
