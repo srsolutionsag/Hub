bd670911e832c8318cc64258631c96d6e1526eae
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2015-05-20 08:43:33 +0200

    Set time limit unlimited to true

--- classes/User/class.hubUser.php
+++ classes/User/class.hubUser.php
@@ -150,7 +150,7 @@ class hubUser extends hubObject {
 //		$this->ilias_object->setDepartment($this->getDepartment());
 //		$this->ilias_object->setFax($this->getFax());
 //		$this->ilias_object->setTimeLimitOwner($this->getTimeLimitOwner());
-//		$this->ilias_object->setTimeLimitUnlimited($this->getTimeLimitUnlimited());
+		$this->ilias_object->setTimeLimitUnlimited($this->getTimeLimitUnlimited());
 //		$this->ilias_object->setTimeLimitFrom($this->getTimeLimitFrom());
 //		$this->ilias_object->setTimeLimitUntil($this->getTimeLimitUntil());
 //		$this->ilias_object->setMatriculation($this->getMatriculation());
