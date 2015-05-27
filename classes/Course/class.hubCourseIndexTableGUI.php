be8654c08cfce0d2f59cd52114de3fa0e3155c98
theodortruffer <tt@studer-raimann.ch>
2015-05-27 14:13:27 +0200

    fixed bug, where query couldnt be executed

--- classes/Course/class.hubCourseIndexTableGUI.php
+++ classes/Course/class.hubCourseIndexTableGUI.php
@@ -95,7 +95,7 @@ class hubCourseIndexTableGUI extends arIndexTableGUI {
         if($name == "parent_id")
         {
             $this->active_record_list->innerjoin("object_reference","parent_id","ref_id",array("obj_id"),"=",true);
-            $this->active_record_list->innerjoin("object_data","object_reference.obj_id","obj_id", array("title AS obj_title"), "=", true);
+            $this->active_record_list->innerjoin("object_data","object_reference.obj_id","obj_id", array("title"), "=", true);
             $this->active_record_list->where("object_data.title like '%" . $value . "%'");
         }
         else{
