2d8c0d023fd09d93a5242853fca828cc1e661b19
Timon Amstutz <timon.amstutz@bluewin.ch>
2015-06-22 18:34:42 +0200

    Fixed GUI-Bug introduced by preparing for 5.0

--- classes/Course/class.hubCourseGUI.php
+++ classes/Course/class.hubCourseGUI.php
@@ -12,18 +12,4 @@ require_once('class.hubCourseDisplayGUI.php');
  *
  */
 class hubCourseGUI  extends arGUI {
-    /**
-     * @param $id
-     */
-    function view($id) {
-        $display_gui_class = $this->record_type . "DisplayGUI";
-        /**
-         * @var arDisplayGUI $display_gui
-         */
-        $display_gui = new $display_gui_class($this, $this->ar->find($id));
-        $this->tpl->setContent($display_gui->getHtml());
-        if(hubConfig::is50()){
-            $this->tpl->show();
-        }
-    }
 }
