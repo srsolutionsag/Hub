252e630c242a12980ff50f71938a0c63518bf54e
Fabian Schmid <fs@studer-raimann.ch>
2016-02-25 16:57:17 +0100

    updated plugin-version

--- plugin.php
+++ plugin.php
@@ -1,11 +1,7 @@
 <?php
-//require_once('./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/Hub/classes/class.ilHubPlugin.php');
-//if (! ilHubPlugin::checkPreconditions()) {}
-
 $id = 'hub';
-$version = '1.1.19';
+$version = '1.2.00';
 $ilias_min_version = '4.3.0';
 $ilias_max_version = '5.0.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
-?>
