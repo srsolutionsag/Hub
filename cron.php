87fb2e339a42153287c644d40a0a3e79fcfbcc53
Robin Baumgartner <rb@studer-raimann.ch>
2016-09-26 11:37:10 +0200

    Prevent cron script to be run as root user

--- cron.php
+++ cron.php
@@ -5,6 +5,10 @@
  *
  * @author Fabian Schmid <fs@studer-raimann.ch>
  */
+if (posix_getuid() == 0) {
+    echo "Running this script as root may lead to filesystem permission issues. Run this script as unprivileged user only. Aborting.\n";
+    exit(1);
+}
 chdir(substr(__FILE__, 0, strpos(__FILE__, '/Customizing')));
 require_once('./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/Hub/classes/Sync/class.hubSyncCron.php');
 hubSyncCron::initAndRun();
