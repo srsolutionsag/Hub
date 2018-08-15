67589347d10022a4b450ce4739db4289f0029498
Martin Studer <ms@studer-raimann.ch>
2018-08-15 08:49:46 +0200

    HUB for ILIAS 5.3

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'hub';
 $version = '2.0.00';
-$ilias_min_version = '5.0.0';
+$ilias_min_version = '5.3.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
