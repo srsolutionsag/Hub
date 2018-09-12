210321b2d34fd493fca96fc877b572064cfcf6e7
Florian <fw@studer-raimann.ch>
2018-09-12 09:58:39 +0200

    Fix team name

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'hub';
 $version = '2.0.00';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.3.999';
-$responsible = 'studer + raimann ag';
+$responsible = 'studer + raimann ag - Team Core 1';
 $responsible_mail = 'support-core1@studer-raimann.ch';
